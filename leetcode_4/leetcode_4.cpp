b823dda4b27b6f92c842e09327c38bd27d038d9e
SZyoo <SZyoo@github>
2021-10-27 10:52:30 +0100

    T:6.22%  M:5.29%

---
+++ leetcode_4/leetcode_4.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() == 0)
+            return 0;
+        int head = 0;
+        int tail = 0;
+        int maxLen = 0;
+        int temp_maxlen = 0;
+        set<char> _set;
+        while (tail < s.length()) {
+            if (_set.find(s[tail]) == _set.end()) {
+                _set.insert(s[tail++]);
+                temp_maxlen++;
+            }
+            else {
+                if (temp_maxlen > maxLen)
+                    maxLen = temp_maxlen;
+                temp_maxlen = 0;
+                while (s[head++] != s[tail]);
+                tail = head;
+                _set.clear();
+            }
+        }
+        if (temp_maxlen > maxLen)
+            maxLen = temp_maxlen;
+        return maxLen;
+    }
+};
+
+int main() {
+    string test = "dvabvcdf";
+    Solution s;
+    cout << s.lengthOfLongestSubstring(test);
+}
