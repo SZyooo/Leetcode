60d463517c680f915c983952e1457af433c695ff
SZyoo <SZyoo@github>
2021-11-03 11:10:28 +0000

    (Problem7)T:87.31%(8ms),M:79.86%(8.3M)

---
+++ leetcode_7/leetcode_7.cpp
@@ -0,0 +1,42 @@
+﻿#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        string answer;
+        int interval = (numRows-1)*2;
+        for (int i = 0; i < numRows; i++)
+        {
+            int k = i;
+            if (k < s.length())
+            {
+                int j = 0;
+                int w;
+                while ((w = k + (j++) * interval) < s.length() )
+                {
+                    answer += s[w];
+                    if (i < numRows - 1 && i > 0)
+                    {
+                        int post = (numRows - 1 - i) * 2;
+                        if (w + post < s.length())
+                        {
+                            answer += s[w + post];
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    } 
+};
+int main()
+{
+    string s("a");
+    cout << Solution().convert(s, 1);
+}
