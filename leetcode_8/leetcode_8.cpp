4bc8a7a92a17abc220d1198e8a3acb0f9c056ad1
SZyoo <SZyoo@github>
2021-11-04 11:25:59 +0000

    (Problem8)T:100%(0ms);M:96.06%(5.8MB)

---
+++ leetcode_8/leetcode_8.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        bool isMinus = x < 0;
+        unsigned int _x = abs(x);
+        int maxBase = 1;
+        while (_x/maxBase > 9) maxBase *= 10;
+        bool willOverflow = (maxBase == 1000000000);
+        int sum = 0;
+        int base = 1;
+        while (maxBase > 1)
+        {
+            sum += base * (_x / maxBase);
+            _x -= _x / maxBase * maxBase;
+            maxBase /= 10;
+            base *= 10;
+        }
+        if (willOverflow)
+        {
+            if (isMinus)
+            {
+                if (sum > 147483648)
+                {
+                    if (_x >= 2) return 0;
+                }
+                else {
+                    if (_x > 2) return 0;
+                }
+            }
+            else
+            {
+                if (sum > 147483647)
+                {
+                    if (_x >= 2) return 0;
+                }
+                else {
+                    if (_x > 2) return 0;
+                }
+            }
+        }
+        sum += _x * base;
+        return isMinus ? -sum : sum;
+    }
+};
+
+int main()
+{
+    int num1 = -2147483648;
+    int num2 = 1234;
+    std::cout << Solution().reverse(num2);
+}
+
