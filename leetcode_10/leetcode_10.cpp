819937078e72bda17562181c8d4f16bc95799efc
SZyoo <SZyoo@github>
2021-11-08 11:01:27 +0000

    (Problem10)T:62.83%(12ms);M:30.85%(6MB)

---
+++ leetcode_10/leetcode_10.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)return false;
+        int sum = 0;
+        int minBase = 1, maxBase = 1;
+        while (x / maxBase > 9)maxBase *= 10;
+        while (minBase < maxBase)
+        {
+            int t_base = (minBase == 1000000000) ? 0 : 10 * (x / (minBase * 10));
+            int left = x / minBase - t_base;
+            t_base = (maxBase == 1000000000) ? 0 : 10 * (x / (maxBase * 10));
+            int right = x / maxBase - t_base;
+            if (left != right)return false;
+            minBase *= 10;
+            maxBase /= 10;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    std::cout << Solution().isPalindrome(456654);
+}
+
+
