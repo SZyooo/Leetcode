0fe1f42faad97f63d8b0e61915ddbdda4717dc08
SZyoo <SZyoo@github>
2021-12-23 10:44:06 +0000

    (33Search in Rotated Sorted Array)T:70.90%(4ms);M:73.82%(11.1MB)

---
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+	inline bool between(int n, int end1, int end2)
+	{
+		if (end1 <= n && n <= end2 || end2 <= n && n <= end1) return true;
+		else return false;
+	}
+	int binarySearch(vector<int>& nums, int start, int end, int target)
+	{
+		if (start == end) return nums[start] == target ? start : -1;
+		if (end == start + 1)
+		{
+			if (nums[start] == target) return start;
+			if (nums[end] == target) return end;
+			else return -1;
+		}
+		int mid = (start + end) / 2;
+		if (nums[mid] == target) return mid;
+		bool left = between(target, nums[start], nums[mid]);
+		bool right = between(target, nums[mid + 1], nums[end]);
+		if (left && right)
+		{
+			if (nums[mid] > nums[end])
+			{
+				return binarySearch(nums, 0, mid - 1, target);
+			}
+			else
+			{
+				return binarySearch(nums, mid + 1, end, target);
+			}
+		}
+		else if (left) return binarySearch(nums, 0, mid - 1, target);
+		else if (right) return binarySearch(nums, mid + 1, end, target);
+		else
+		{
+			if (nums[mid] > nums[end])
+			{
+				return binarySearch(nums, mid+1, end, target);
+			}
+			else
+			{
+				return binarySearch(nums, start, mid-1, target);
+			}
+		}
+	}
+	int search(vector<int>& nums, int target) {
+		return binarySearch(nums, 0, nums.size() - 1, target);
+	}
+};
+
+int main()
+{
+	vector<int> test{ 0,1,2,3,4,6,7 };
+	cout << Solution().search(test, 4);
+}
