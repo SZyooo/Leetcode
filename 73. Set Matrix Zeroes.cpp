cdf10da6ce9f44fadb51b4ab8e881cb5445e7814
SZYoo <18742024073@163.com>
2022-02-14 20:15:30 +0000

    72-73

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int> rows, columns;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    rows.insert(i);
+                    columns.insert(j);
+                }
+            }
+        }
+        for(auto row : rows)
+        {
+            for(int i=0;i<matrix[0].size();i++)
+            {
+                matrix[row][i] = 0;
+            }
+        }
+        for(auto column:columns)
+        {
+            for(int i=0;i<matrix.size();i++)
+            {
+                matrix[i][column] = 0;
+            }
+        }
+    }
+};
